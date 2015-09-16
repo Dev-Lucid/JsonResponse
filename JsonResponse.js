0e7ad75a174fffe2f22afd3a5a2836db8b8fb174
Mike Thorn <mike@TherapyCharts.com>
2015-09-16 15:24:43 -0400

    adding working code

--- JsonResponse.js
+++ JsonResponse.js
@@ -1,2 +1,56 @@
 
-var jsonResponse = {};
+function jsonResponse(newHash,defaultUrl){
+    var url = new String(newHash);
+    url = url.substring(2,url.length); // remove #!
+    if(url == ''){ // use the default url if this is empty
+        url = defaultUrl;
+    };
+    url = url.split('|'); // split up the url into the todo and the paramters
+    var data = {};
+    data['todo'] = url.shift();  // shift off the todo
+    while(url.length > 0){ // loop over the remaining elements and transform into an associative array
+        data[url.shift()] = url.shift();
+    };
+
+    // send it on up via ajax! :D
+    jQuery.ajax('app.php',{
+        'data':data,
+        'success':function(response){
+            var js = '';
+            for(var key in response){
+                switch(key){
+                    case 'title': 
+                        document.title = response[key]; 
+                        break;
+                    case 'keywords': 
+                        $('meta[name=keywords]').attr('content',response[key]);
+                        break;
+                    case 'description': 
+                        $('meta[name=description]').attr('content',response[key]);
+                        break;
+                    case 'javascript': 
+                        js = response[key]; 
+                        break;
+                    case 'append': 
+                        for(var selector in response[key]){
+                            jQuery(selector).append(response[key][selector]);
+                        }
+                        break;
+                    case 'prepend': 
+                        for(var selector in response[key]){
+                            jQuery(selector).prepend(response[key][selector]);
+                        }
+                        break;
+                    case 'replace': 
+                        for(var selector in response[key]){
+                            jQuery(selector).html(response[key][selector]);
+                        }
+                        break;
+                }
+            }
+            if (js !=''){
+                eval(js);
+            }
+        }
+    });
+};
--- JsonResponse.php
+++ JsonResponse.php
@@ -4,8 +4,97 @@ namespace DevLucid;
 
 class JsonResponse
 {
-    public static function init()
+    private static $instance;
+
+    public function __construct()
     {
-        error_log("called\n");
+        $this->clear();
+        $this->logger = null;
+    }
+
+    public static function get_instance($config=[])
+    {
+        if (null !== static::$instance) {
+             return static::$instance;
+        }
+        static::$instance = new JsonResponse();
+        foreach ($config as $key=>$value)
+        {
+            static::$instance->$key = $value;
+        }
+
+        return static::$instance;
+    }
+
+    public function title($title)
+    {
+        $this->response['title'] = $title;
+    }
+
+    public function description($description)
+    {
+        $this->response['description'] = $description;
+    }
+
+    public function keywords($keywords)
+    {
+        $this->response['keywords'] = $keywords;
+    }
+
+    public function javascript($javascript)
+    {
+        $this->response['javascript'] .= $javascript;
+    }
+
+    public function prepend($selector, $content=null)
+    {
+        if (!isset($this->response['prepend'][$selector]))
+        {
+            $this->response['prepend'][$selector] = '';
+        }
+        $this->response['prepend'][$selector] .= (string) $content;
+    }
+
+    public function append($selector, $content=null)
+    {
+        if (!isset($this->response['append'][$selector]))
+        {
+            $this->response['append'][$selector] = '';
+        }
+        $this->response['append'][$selector] .= (string) $content;
+    }
+
+    public function replace($selector, $content=null)
+    {
+        if (!isset($this->response['replace'][$selector]))
+        {
+            $this->response['replace'][$selector] = '';
+        }
+        $this->response['replace'][$selector] .= (string) $content;
+    }
+
+    public function clear()
+    {
+        $this->response = [
+            'title'       => null,
+            'description' => null,
+            'keywords'    => null,
+            'javascript'  => '',
+            'prepend'     => [],
+            'append'      => [],
+            'replace'     => [],
+        ];
+    }
+
+    public function send()
+    {
+        header('Content-Type: application/json');
+        header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+        header("Cache-Control: no-store, no-cache, must-revalidate"); 
+        header("Cache-Control: post-check=0, pre-check=0", false);
+        header("Pragma: no-cache"); // HTTP/1.0
+        header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
+        echo(json_encode(array_filter($this->response)));
+        exit();
     }
 }
